d6f3cf36c88377d6d7ee7b633fb0f44d64e29309
Amir Orbe <orbebrian@gmail.com>
2021-04-21 15:35:44 -0500

    se corrigio la funcion filter y se agrego la funcion build_pixel_map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,19 @@ defmodule Identicon do
     |> hash_input
     |> pic_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map do
+    
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) -> 
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
